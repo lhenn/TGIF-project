3468317eaabe8cff3c57009ebcc67eda476175d6
Laura Henn <hennlaura1@gmail.com>
2019-03-16 12:07:42 +0100

    state-filter now working

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 
 //------------------Congress 113 Senate/House Pages-------------------------//
 var membersPath = congressData.results[0].members;
+
+//party checkboxes
 var democratFilter = document.getElementById("democratFilter");
 var republicanFilter = document.getElementById("republicanFilter");
 var independentFilter = document.getElementById("independentFilter");
@@ -18,15 +20,21 @@ for(var i = 0; i < partyFilters.length; i++) {
 }
 
 stateFilter.addEventListener("change", function() {
+  createStateTags("none");
   createTable(filter());
 })
 
 populateStateOptions();
+
+//ensure all states are selected when loading page
+stateFilter.selectedIndex = "0";
+createStateTags("none");
+
 createTable(membersPath);
 
-function populateStateOptions() {
+//get all unique states from unfiltered members data
+function populateStateOptions(states) {
   var states = [];
-  var select = document.getElementById("stateFilter");
   for (var i = 0; i < membersPath.length; i++) {
     if (!states.includes(membersPath[i].state)) {
       states.push(membersPath[i].state);
@@ -34,6 +42,7 @@ function populateStateOptions() {
   }
   states = states.sort();
   states.unshift("All");
+  var select = document.getElementById("stateFilter");
   for (var i = 0; i < states.length; i++) {
     var option = document.createElement('option');
     option.value = states[i];
@@ -42,8 +51,55 @@ function populateStateOptions() {
   }
 }
 
-//ensure all states are selected when loading page
-stateFilter.selectedIndex = "0";
+// This function creates tags based on whch states are selected. It's argument takes
+// an exception, which is generated when a state tag is exed-out. If you are just adding
+// states and not removing, the argument should be "none".
+function createStateTags(exception) {
+  tagDiv = document.getElementById("state-tag-container");
+  var existingTags = Array.from(document.getElementsByClassName("state-name")).map(elt => elt.innerHTML);
+  var selectedStates = Array.from(document.querySelectorAll('#stateFilter option:checked')).map(opt => opt.value);
+  // in the case that a tag has been ex'd out:
+  if (exception != "none") {
+    tagDiv.innerHTML = "";
+    selectedStates = existingTags;
+    var exceptionIndex = selectedStates.indexOf(exception);
+    selectedStates.splice(exceptionIndex, 1);
+  }
+  console.log("selectedStates: ", selectedStates);
+  if (selectedStates.includes("All")) {
+      tagDiv.innerHTML = "";
+      selectedStates = Array.from(document.querySelectorAll('#stateFilter option')).map(opt => opt.value);
+      //get rid of "all" in the array
+      selectedStates.splice(0, 1);
+    }
+  for (var i = 0; i < selectedStates.length; i++) {
+    if ( (exception == "none" && !existingTags.includes(selectedStates[i]) ) ||
+    exception != "none") {
+      var stateTag = document.createElement("div");
+      stateTag.className = "state-tag";
+      var stateName = document.createElement("div");
+      stateName.className = "state-name";
+      var tagX = document.createElement("button");
+      tagX.className = "tag-x";
+      tagX.addEventListener("click", function() {
+        removeTag();
+      });
+      stateName.innerHTML = selectedStates[i];
+      tagX.innerHTML = "x";
+      stateTag.appendChild(stateName);
+      stateTag.appendChild(tagX);
+      tagDiv.appendChild(stateTag);
+    }
+  }
+}
+
+function removeTag() {
+  console.log(event.target.parentElement);
+  var stateTag = event.target.parentElement;
+  var removedStateName = stateTag.childNodes[0].innerText;
+  createStateTags(removedStateName);
+  createTable(filter());
+}
 
 // accepts array of members objects (e.g. membersPath or filtered version)
 function createTable(data) {
@@ -77,97 +133,14 @@ function filter() {
   filteredMembersArray = [];
   //get all checked party values
   partyFilters = Array.from(document.querySelectorAll('input[name=party]:checked')).map(elt => elt.value);
-  //get all selected state values
-  stateFilters = Array.from(document.querySelectorAll('#stateFilter option:checked')).map(opt => opt.value);
+  //get all selected state values from existing state tags
+  var stateFilters = Array.from(document.getElementsByClassName("state-name")).map(elt => elt.innerHTML);
+  console.log("stateFilters: ", stateFilters);
   for (var i = 0; i < membersPath.length; i++) {
       if (partyFilters.includes(membersPath[i].party) &&
       (stateFilters.includes(membersPath[i].state) || stateFilters.includes("All"))) {
         filteredMembersArray.push(membersPath[i]);
-        console.log(membersPath[i].party, membersPath[i].state);
       }
     }
   return filteredMembersArray;
 }
-
-//------------------Attendance and Party Loyalty Pages-------------------------//
-
-var democratMembers = [];
-var republicanMembers = [];
-var independentMembers = [];
-
-for(var i = 0; i < membersPath.length; i++){
-  if(membersPath[i].party == "D"){
-    democratMembers.push(membersPath[i]);
-  }
-  if(membersPath[i].party == "R"){
-    republicanMembers.push(membersPath[i]);
-  }
-  if(membersPath[i].party == "I"){
-  independentMembers.push(membersPath[i]);
-  }
-}
-
-var statistics = {
-    "numberOfDemocrats":democratMembers.length,
-    "numberOfRepublicans":republicanMembers.length,
-    "numberOfIndependents":independentMembers.length,
-    "averageDemocratLoyalty":calculateAverageLoyalty("D"),
-    "averageRepublicanLoyalty":calculateAverageLoyalty("R"),
-    "mostMissedVotes":getPercent("missed_votes_pct","top",.1),
-    "leastMissedVotes":getPercent("missed_votes_pct","bottom",.1),
-    "mostLoyal":getPercent("votes_with_party_pct","top",.1),
-    "leastLoyal":getPercent("votes_with_party_pct","bottom",.1)
-}
-
-function dynamicSort(property) {
-  var sortOrder = 1;
-  if(property[0] === "-") {
-    sortOrder = -1;
-    property = property.substr(1);
-  }
-  return function (a,b) {
-    var result = (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0;
-    return result * sortOrder;
-  }
-}
-
-function calculateAverageLoyalty(party) {
-  var loyaltyPercentageArray = []
-  for(var i = 0; i < membersPath.length; i++) {
-    if(party == membersPath[i].party){
-      // console.log(membersPath[i].votes_with_party_pct);
-      loyaltyPercentageArray.push(membersPath[i].votes_with_party_pct);
-    }
-  }
-  average = loyaltyPercentageArray.reduce(function(a,b){return a + b}, 0) / loyaltyPercentageArray.length;
-  return average;
-}
-
-function getPercent(property, direction, percent) {
-  var percentArray = [];
-  if (direction == "top") {
-    var sortedMembers = [...membersPath].sort(dynamicSort("-" + property));
-  }
-  if (direction == "bottom") {
-    var sortedMembers = [...membersPath].sort(dynamicSort(property));
-  }
-  for (var i = 0; i < sortedMembers.length; i++) {
-    if ((i + 1) / sortedMembers.length <= percent) {
-      percentArray.push(sortedMembers[i]);
-    }
-    else {
-      if (sortedMembers[i][property] == sortedMembers[i - 1][property]) {
-        percentArray.push(sortedMembers[i]);
-      }
-      else {
-        break;
-      }
-    }
-  }
-  return percentArray;
-}
-
-
-
-//scratch area
-console.log(statistics);
